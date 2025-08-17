8ededb28ccb1721ce9222620b6d12566255457d3
rarodev <robinsonabra24@gmail.com>
2025-08-16 23:29:06 -0500

    react app ready

--- index.js
+++ index.js
@@ -2,39 +2,43 @@ import express from "express";
 import dotenv from "dotenv";
 import auth from "./routes/auth.js";
 import events from "./routes/events.js";
-
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import dbConnection from "./database/config.js";
 import cors from "cors";
 
 dotenv.config();
 
-//crear el servidor de express
+// Definir __dirname en ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+// Crear el servidor de express
 const app = express();
 
-//Base de datos
+// Base de datos
 dbConnection();
 
-//cors
-
+// Middlewares
 app.use(cors());
-// escuchar
-
-//Directorio publico
-
+app.use(express.json());
 app.use(express.static("public"));
 
-app.use(express.json());
-
-//Todo?: rutas atuh // crear / login / register / refresh / logout / renew
-
+// Rutas principales
 app.use("/api/auth", auth);
 app.use("/api/events", events);
 
-//puerto
-const port = process.env.PORT || process.env.PORT;
+// SPA fallback
+// SPA fallback (funciona en Express 5)
+app.get(/^(?!\/api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-//escuchar
+
+// Puerto
+const port = process.env.PORT || 4000;
+
+// Escuchar peticiones
 app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
+  console.log(`🚀 Servidor corriendo en el puerto ${port}`);
 });
